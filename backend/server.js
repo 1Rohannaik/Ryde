e795a87eab8a2f1709d89f580d7e53e038909193
Rohan Naik <139874770+1Rohannaik@users.noreply.github.com>
2025-08-24 13:09:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const http = require("http"); // 👉 Add this
+const http = require("http");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
@@ -8,23 +8,20 @@ const cookieParser = require("cookie-parser");
 const errorHandler = require("./lib/error");
 const sequelize = require("./lib/db");
 
-
 // 👉 Import your socket initializer
 const { initSocket } = require("./socket");
 
+// Routes
 const mapRoutes = require("./src/routes/mapRoutes");
-const rideRouts = require("./src/routes/rideRoutes");
+const rideRoutes = require("./src/routes/rideRoutes");
 const authRoutes = require("./src/routes/authRoutes");
 const captainRoutes = require("./src/routes/captainRoutes");
-const paymentRoutes = require("./src/routes/paymentRoutes")
+const paymentRoutes = require("./src/routes/paymentRoutes");
 
-// cors config
+// ✅ CORS config (important for cookies)
 app.use(
   cors({
-    origin: [
-      "https://ryde-j1ba.onrender.com",
-      " ",
-    ],
+    origin: "https://ryde-j1ba.onrender.com", // your frontend
     credentials: true,
   })
 );
@@ -33,10 +30,11 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 app.use("/api/v1/users", authRoutes);
 app.use("/api/v1/captain", captainRoutes);
 app.use("/api/v1/maps", mapRoutes);
-app.use("/api/v1/ride", rideRouts);
+app.use("/api/v1/ride", rideRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 
 // Global error handler
