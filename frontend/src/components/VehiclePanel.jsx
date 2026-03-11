4086f9480b1587086713d8cd0f64dd11176c7e77
Rohan Naik <139874770+1Rohannaik@users.noreply.github.com>
2026-03-11 20:06:39 +0530

    Update vehicle images to new Cloudinary URLs

--- frontend/src/components/VehiclePanel.jsx
+++ frontend/src/components/VehiclePanel.jsx
@@ -103,7 +103,7 @@ const VehiclePanel = ({
         >
           <img
             className="h-14 w-14 object-cover rounded-lg"
-            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_638,w_956/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png"
+            src="https://res.cloudinary.com/dn2hxlyql/image/upload/v1773238923/RydeMoto.webp"
             alt="Moto"
           />
           <div className="ml-4 flex-1">
@@ -128,7 +128,7 @@ const VehiclePanel = ({
         >
           <img
             className="h-14 w-14 object-cover rounded-lg"
-            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png"
+            src="https://res.cloudinary.com/dn2hxlyql/image/upload/v1773239616/Uber_Auto_312x208_pixels_Mobile_o8xxrq.png"
             alt="RydeAuto"
           />
           <div className="ml-4 flex-1">
