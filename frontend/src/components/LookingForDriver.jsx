5865741d78b7f34e1d74b5009d4a0c9c9c174f1e
Rohan Naik <139874770+1Rohannaik@users.noreply.github.com>
2026-03-11 20:19:26 +0530

    Change vehicle images in LookingForDriver component
    Updated image URLs for moto and auto vehicle types.

--- frontend/src/components/LookingForDriver.jsx
+++ frontend/src/components/LookingForDriver.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 const LookingForDriver = (props) => {
-  // Map vehicleType to corresponding image URLs
   const vehicleImages = {
     car: "https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg",
-    moto: "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_638,w_956/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png",
-    auto: "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png",
+    moto: "https://res.cloudinary.com/dn2hxlyql/image/upload/v1773238923/RydeMoto.webp",
+    auto: "https://res.cloudinary.com/dn2hxlyql/image/upload/v1773239616/Uber_Auto_312x208_pixels_Mobile_o8xxrq.png",
   };
 
   // Map vehicleType to display names
